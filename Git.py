8ee84af87166630b72e4ec84690bed011dfc0e50
@Warp-speed123 <shyam7mancherial@gmail.com>
2025-09-28 10:14:40 +0100

    changing full stop to exclamation mark!

--- Git.py
+++ Git.py
@@ -1,5 +1,5 @@
 def load_up():
-    print("Wow, we have this project up on git hub and now we can work with many people.")
+    print("Wow, we have this project up on git hub and now we can work with many people!")
 
 
 load_up()
